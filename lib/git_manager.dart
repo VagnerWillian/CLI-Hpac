c1ea9c80fe0a3b82271917fb20b6f650c7841f65
Vagner Willian <tec_vagner.ti@outlook.com>
2022-10-07 05:48:04 -0300

    teste new package

--- lib/git_manager.dart
+++ lib/git_manager.dart
@@ -1,6 +1,8 @@
 
 import 'dart:io';
+import 'package:git/git.dart' as git;
 
 Future addAll()async{
-  await Process.start('git', ["add", "*"], runInShell: true, mode: ProcessStartMode.detached);
+  // await Process.start('git', ["add", "*"], runInShell: true, mode: ProcessStartMode.detached);
+  await git.runGit(['add', '*']);
 }
