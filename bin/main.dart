c425b264151e294561361e824f45b7636f93a4f5
Vagner Willian <tec_vagner.ti@outlook.com>
2022-10-07 05:52:35 -0300

    testing package git

--- bin/main.dart
+++ bin/main.dart
@@ -6,7 +6,7 @@ import 'package:hpac/set_project.dart' as setProject;
 import 'package:hpac/config_manager.dart' as config_manager;
 import 'package:hpac/git_manager.dart' as git_manager;
 
-void main(List<String> arguments) async{
+void main(List<String> arguments) async {
   var parser = ArgParser();
   var command = ArgParser();
   parser.addCommand('help', command);
@@ -14,30 +14,30 @@ void main(List<String> arguments) async{
   parser.addCommand('clear', command);
   parser.addCommand('add', command);
   parser.addOption('set', abbr: 's');
-  var args  = parser.parse(arguments);
+  var args = parser.parse(arguments);
 
   print("•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••");
   print("••••••••••••••••••••••••HPAC CLI••••••••••••••••••••••••••••••••");
   print("••••••••••••DESENVOLVIDO POR HAMMER CONSULT•••••••••••••••••••••");
   print("•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••");
-  if(args.command?.name=='add'){
+  if (args.command?.name == 'add') {
     await setProject.run(clientDefault);
     print("GIT ADD * ...");
     await git_manager.addAll();
-  }else if(args.command?.name=='help'){
+  } else if (args.command?.name == 'help') {
     print("-s(or --set) \"<client-name>\"");
     print("info • for actually selected client");
     print("help • help for commands");
     print("clear • for clear archive of configuration");
-  }else if(args.command?.name=='info'){
+  } else if (args.command?.name == 'info') {
     String status = await config_manager.readStatusConfigFile();
-    print("•••••••••••• CLIENTE SELECIONADO ${status.toUpperCase()} •••••••••••••••••••••");
-  }else if(args.command?.name=='clear'){
+    print("•••••••••••• CLIENTE SELECIONADO ${status
+        .toUpperCase()} •••••••••••••••••••••");
+  } else if (args.command?.name == 'clear') {
     config_manager.clear();
-  }else if(args.options.contains("set")){
+  } else if (args.options.contains("set")) {
     setProject.run(args['set']);
-  }else {
+  } else {
     print("???????? COMANDO DESCONHECIDO ?????????");
   }
-
 }
