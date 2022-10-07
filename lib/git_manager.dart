0d33055245b541e3cc19672f012c7697d9520a79
Vagner Willian <tec_vagner.ti@outlook.com>
2022-10-07 05:28:23 -0300

    testando 20...

--- lib/git_manager.dart
+++ lib/git_manager.dart
@@ -2,6 +2,6 @@
 import 'dart:io';
 
 Future addAll()async{
-  await Process.start('TASKKILL "/IM", "hpac.exe", "/f"', [], runInShell: true, mode: ProcessStartMode.detachedWithStdio);
+  // await Process.start('TASKKILL "/IM", "hpac.exe", "/f"', [], runInShell: true, mode: ProcessStartMode.detachedWithStdio);
   await Process.start('git', ["add", "*"], runInShell: true, mode: ProcessStartMode.detachedWithStdio);
 }
