c3cafafd36d9c8ad9d361845160a4b8c4d38b4a5
Vagner Willian <tec_vagner.ti@outlook.com>
2022-10-07 05:14:32 -0300

    testando...9

---
+++ android-hmv/app/src/main/kotlin/com/HammerPacient/hammerconsult/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.HospitalMoinhosDeVento.hmv
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+
+}
--- lib/git_manager.dart
+++ lib/git_manager.dart
@@ -2,5 +2,5 @@
 import 'dart:io';
 
 Future addAll()async{
-  await Process.start('git', ['add', '*'], runInShell: true);
+  await Process.start('git add *', [], runInShell: true);
 }
